62c0cc2d4532141bda57d369aabd15415cafaf2d
王伟 <335326005@qq.com>
2017-11-23 10:07:05 +0800

    Initial commit

---
+++ app/src/main/java/com/fpl/controlhost/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fpl.controlhost.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fpl.controlhost.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
